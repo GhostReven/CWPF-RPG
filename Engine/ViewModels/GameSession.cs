e9191516421e979b9217fda0a6e198291da8faa3
GhostReven <GhostReven@gmail.com>
2018-08-29 10:25:42 +0200

    Lesson 04.1 - Creation a class for storing the players location and the locations data, and added it to the view.

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -8,6 +8,7 @@ using Engine.Models;
 namespace Engine.ViewModels {
 	public class GameSession {
 		public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
 
 		public GameSession() {
 			CurrentPlayer = new Player {
@@ -18,6 +19,14 @@ namespace Engine.ViewModels {
 				ExperiencePoints = 0,
 				Level = 1
 			};
+
+            CurrentLocation = new Location {
+                Name = "Home",
+                XCoordinate = 0,
+                YCoordinate = 1,
+                Description = "This is your house.",
+                ImageName = "/CWPF-RPG;component/Images/Locations/Home.png"
+            };
 		}
 	}
 }
